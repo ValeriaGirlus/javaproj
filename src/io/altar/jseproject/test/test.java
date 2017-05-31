93592314a5469f4827d484aa9e28b61162c29e58
ValeriaGirlus <valeriagirlus@gmail.com>
2017-05-31 13:19:26 +0100

    menu product e shelf

--- src/io/altar/jseproject/test/test.java
+++ src/io/altar/jseproject/test/test.java
@@ -7,6 +7,7 @@ public class test {
 	public static void main(String[] args) {
 		TextInterface.menu_principal();
 		
+		
 	}		
 		
 
--- src/io/altar/jseproject/textinterface/TextInterface.java
+++ src/io/altar/jseproject/textinterface/TextInterface.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TextInterface {
 
 	public static void menu_principal() {
-		System.out.println("Por favor seleccione uma das seguintes opções:");
+		System.out.println("Por favor seleccione uma das seguintes opï¿½ï¿½es:");
 		System.out.println("1)Listar produtos");
 		System.out.println("2)Listar prateleiras");
 		System.out.println("3)Sair");
@@ -16,41 +16,70 @@ public class TextInterface {
 			TextInterface.menu_product();
 			break;
 		case 2:
-			TextInterface.menu_prateleira();
+			TextInterface.menu_shelf();
 			break;
+			
 			
 		}
 		
 	}
 
 	public static void menu_product() {
-		System.out.println("Por favor seleccione uma das seguintes opções:");
+		System.out.println("Por favor seleccione uma das seguintes opcoes:");
 		System.out.println("1)Criar novo produto");
 		System.out.println("2)Editar um produto existente");
 		System.out.println("3)Consultar o detalhe do produto");
 		System.out.println("4)Remover um produto");
-		System.out.println("5)Voltar ao ecrã anterior");
+		System.out.println("5)Voltar ao ecra anterior");
 		
 		int input = getInput(1,5);
 		switch(input){
 		case 1:
-			
+			createProduct();
 			break;
 		case 2: 
+			editProduct();
 			break;
 		case 3:
+			consultProduct();
 			break;
+		case 4:
+			removeProduct();
+			break;
+		case 5:
+			TextInterface.menu_principal();
+			break;
+
 		}
 		
 	}
 
-	public static void menu_prateleira() {
+	public static void menu_shelf() {
 		System.out.println("Por favor seleccione uma das seguintes opcoes:");
 		System.out.println("1)Criar nova prateleira");
 		System.out.println("2)Editar uma prateleira existente");
 		System.out.println("3)Consultar o detalhe da prateleira");
 		System.out.println("4)Remover uma prateleira");
-		System.out.println("5)Voltar ao ecrã anterior");
+		System.out.println("5)Voltar ao ecra anterior");
+		
+		int input = getInput(1,5);
+		switch(input){
+		case 1:
+			createShelf();
+			break;
+		case 2: 
+			editShelf();
+			break;
+		case 3:
+			consultShelf();
+			break;
+		case 4:
+			removeShelf();
+			break;
+		case 5:
+			TextInterface.menu_principal();
+			break;
+		}
 	}
 	
 	public static int getInput(int min, int max) {
@@ -63,15 +92,53 @@ public class TextInterface {
 				if (input >= min && input <= max) {
 					return input;
 				} else {
-					System.out.println("Por favor introduza um número entre " + min + " e " + max);
+					System.out.println("Por favor introduza um numero entre " + min + " e " + max);
 				}
 			} else {
-				System.out.println("Por favor introduza um número");
+				System.out.println("Por favor introduza um numero");
 				s.next();
-			}
-		}
-	}
+				s.close();
 
+			}
+	
+		}
+		
+	}
+	
+	public static void createProduct(){
+		Product 
+		System.out.println("");
+		
+	}
+	
+	public static void editProduct(){
+		System.out.println("2)Editar um produto existente");
+	}
+	
+	public static void consultProduct(){
+		System.out.println("3)Consultar o detalhe do produto");
+	}
+	
+	public static void removeProduct(){
+		System.out.println("4)Remover um produto");
+	}
+	
+	public static void createShelf(){
+		System.out.println("1)Criar nova prateleira");
+	}
+	
+	public static void editShelf(){
+		System.out.println("2)Editar uma prateleira existente");
+	}
+	
+	public static void consultShelf(){
+		System.out.println("3)Consultar o detalhe da prateleira");
+	}
+	
+	public static void removeShelf(){
+		System.out.println("4)Remover uma prateleira");
+	}
+	
 }
 			
 				
