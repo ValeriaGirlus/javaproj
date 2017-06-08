66796b1c999f8fbbb8daea0a0255c9bda7ad2035
ValeriaGirlus <valeriagirlus@gmail.com>
2017-06-08 13:20:18 +0100

    faltava isto

--- src/io/altar/jseproject/Repository/ProductRepository.java
+++ src/io/altar/jseproject/Repository/ProductRepository.java
@@ -2,9 +2,6 @@ package io.altar.jseproject.Repository;
 
 import io.altar.jseproject.model.Product;
 
-
-
-
 public class ProductRepository extends EntityRepository<Product>{
 	
 	private static final ProductRepository INSTANCE = new ProductRepository();
